3639d4e16adb6031cdebcfc34e86a4e871de3dee
alexandergogl <gogl.alexander@gmail.com>
2018-12-19 00:27:30 +0100

    Added an unembed method

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
 # SkimPDF
 
-Python script to batch embed Skim notes to PDFs. I used it to embed 568 pdfs scattered in a folder with various subfolders. It took 300 seconds. Be aware, that the script won't process files with a `"` in its filepath.
+[Skim](https://skim-app.sourceforge.io/) is a PDF viewer with great search and annotation tools for OSX. It stores annotations in a non-standard format, thus the annotation can not viewed or edited in other PDF viewer like Adobe Acrobat Reader. However, Skim can convert Skim notes to standard PDF notes (embed) or make PDF notes editable in Skim (unembed).
 
+![](img/example-skim.png)
+PDF viewed in Skim with Skim annotations.
 
+![](img/example-acrobat.png)
+The same PDF with embedded annotations viewed in Adobe Acrobat Reader DC.
 
-### Available options
+The Python class `SkimPDF` builds on `skimpdf` to automate the conversion step:
 
-* Replace pdf or place a copy in place: `skim.replace = True`
+1. Embed notes
+2. Unembed notes
+3. Batch embed and unembed notes (works with nested folders; see below)
 
-* Set the embed suffix (if `skim.replace` is `False`): `skim.embed_suffix = '_embeded'`
+## Methods
 
-### Functions
+* Convert Skim notes to PDF notes (embed): `skim.convert_to_pdf_notes('../path/to/pdf file.pdf')`
+* Convert PDF notes to Skim notes (unembed): `skim.convert_to_skim_notes('../path/to/pdf file.pdf')`
+* Batch convert notes in a folder: `skim.batch_convert_to_pdf_notes('../path/to/Literature folder')` and `skim.batch_convert_to_skim_notes('../path/to/Literature folder')`
 
-* Embed notes of a simgle pdf: `skim.embed_notes('../path/to/pdf file.pdf')`
+## Batch processing
 
-* Batch embed notes to pdfs in folder: `skim.embed_notes_batch('../path/to/Literature folder')`
+The methods `batch_convert_to_pdf_notes` and `batch_convert_to_skim_notes` batch embed and unembed notes. I have used it to embed Skim notes of 568 pdfs scattered in a folder with various subfolders. It took 5 minutes in total. Be aware, that the script won't process files with a `"` in its filepath.
+
+## Available options
+
+* Replace pdf or place a copy in place: `skim.replace_original = True` (default)
+* If `skim.replace_original` is set to `False`, then you can set the embed or unembed suffix with `skim.embed_suffix` and `skim.unembed_suffix`
+
+## Alfred Workflows
+
+For ease of use, I have wrapped the python script in the [Alfred Workflows](https://www.alfredapp.com/) `PDF to Skim notes.alfredworkflow` and `Skim to PDF notes.alfredworkflow` to process one or more selected PDFs.
+
+**WARNING:** The Alfred Workflows overwrite the selected files.
--- scr/skim_pdf.py
+++ scr/skim_pdf.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+skim_pdf.
+
+Embed Skim notes as PDF notes or convert PDF notes to Skim notes.
+Date: 2018-12-18
+Version: 1.1
+Author: Alexander Gogl
+"""
 
 from os import system, walk, path
 
@@ -7,13 +15,14 @@ class SkimPDF(object):
     """docstring for SkimPDF."""
     def __init__(self):
         self.skimpdf_path = '/Applications/Skim.app/Contents/SharedSupport/skimpdf'
-        self.embed_suffix = '_embeded'
-        self.replace = False
+        self.embed_suffix = '_embedded'
+        self.unembed_suffix = '_skim_notes'
+        self.replace_original = True
         pass
 
-    def embed_notes(self, in_pdf):
-        """Embed skim notes to PDF."""
-        if self.replace is False:
+    def convert_to_pdf_notes(self, in_pdf):
+        """Embed skim notes to PDF notes."""
+        if self.replace_original is False:
             out_pdf = "%s%s.pdf" % (in_pdf[:-4], self.embed_suffix)
         else:
             out_pdf = in_pdf
@@ -25,13 +34,13 @@ class SkimPDF(object):
 
         # Compose message
         if result == 0:
-            message = "Embeded notes to '%s'" % in_pdf
+            message = "Embeded notes to '%s'" % out_pdf
         else:
             message = result
 
         return message
 
-    def embed_notes_batch(self, folder):
+    def batch_convert_to_pdf_notes(self, folder):
         """Loop through directories in given folder and embed notes."""
         messages = []
         i = 0
@@ -41,7 +50,46 @@ class SkimPDF(object):
                     i += 1
                     # embed notes to pdf
                     pdf_file = "%s/%s" % (path, name)
-                    result = skim.embed_notes(pdf_file)
+                    result = skim.convert_to_pdf_notes(pdf_file)
+                    # add result message to report
+                    messages.append(result)
+                    # report current state
+                    print(i)
+
+        self.report(messages)
+        pass
+
+    def convert_to_skim_notes(self, in_pdf):
+        """Convert PDF notes to Skim notes (unembed)."""
+        if self.replace_original is False:
+            out_pdf = "%s%s.pdf" % (in_pdf[:-4], self.skim_suffix)
+        else:
+            out_pdf = in_pdf
+
+        # Convert PDF notes to Skim notes
+        cmd = '%s unembed "%s" "%s"' % (self.skimpdf_path, in_pdf, out_pdf)
+
+        result = system(cmd)
+
+        # Compose message
+        if result == 0:
+            message = "Converted PDF notes to Skim notes in '%s'" % out_pdf
+        else:
+            message = result
+
+        return message
+
+    def batch_convert_to_skim_notes(self, folder):
+        """Loop through directories in given folder and embed notes."""
+        messages = []
+        i = 0
+        for path, subdirs, files in walk(folder):
+            for name in files:
+                if name.endswith(".pdf"):
+                    i += 1
+                    # embed notes to pdf
+                    pdf_file = "%s/%s" % (path, name)
+                    result = skim.convert_to_skim_notes(pdf_file)
                     # add result message to report
                     messages.append(result)
                     # report current state
@@ -61,7 +109,10 @@ class SkimPDF(object):
 
 
 skim = SkimPDF()
-skim.replace = True
+skim.replace_original = False
+
+skim.embed_notes('../test/test.pdf')
+# skim.convert_to_skim_notes('../test/test.pdf')
 
 # batch embeding process with path to folder with literature
-skim.embed_notes_batch('../Literature')
+# skim.batch_convert_to_pdf_notes('../test')
